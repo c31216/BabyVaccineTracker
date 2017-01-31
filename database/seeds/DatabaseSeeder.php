d27339564ee12b4ac433be01053c1ee49df1efbb
c31216 <c31216@gmail.com>
2017-01-31 22:47:34 +0800

    s

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-     
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         $this->call(VaccinesTableSeeder::class);
         $this->call(PatientsTableSeeder::class);
 
-      
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
